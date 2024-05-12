a3aca5789f250de4e91adf94f9035572b857f08c
aster <137767097+aster-void@users.noreply.github.com>
2024-05-12 19:16:10 +0900

    initial commit

---
+++ psql/run.sh
@@ -0,0 +1,7 @@
+!/usr/bin/env bash
+# docker run -it --rm psql
+docker run -d --name psql -p 127.0.0.1:5432:5432 \
+  -e POSTGRES_PASSWORD=password \
+  -e POSTGRES_USER=postgres \
+  -e POSTGRES_DB=database \
+  postgres
