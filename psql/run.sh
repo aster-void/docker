dc299219f292990e99b51044de65dc3787cc83c8
aster <137767097+aster-void@users.noreply.github.com>
2024-12-17 19:43:58 +0900

    doc(psql): add DATABASE_URL

--- psql/run.sh
+++ psql/run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# it will launch a postgres instance at
+# DATABASE_URL=postgres://user:password@localhost:5432/database
 docker run -d --rm --name postgres -p 5432:5432 \
   -e POSTGRES_PASSWORD=password \
   -e POSTGRES_USER=user \
